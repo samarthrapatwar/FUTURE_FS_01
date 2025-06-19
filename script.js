260d3013dbe60cae6cd17fcd97166ebd0275da90
samarthrapatwar <160481264+samarthrapatwar@users.noreply.github.com>
2025-06-19 17:05:24 +0530

    finished!

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = new FormData(form);
+
+  fetch("/", {
+    method: "POST",
+    body: formData,
+  })
+    .then(() => {
+      form.reset();
+      document.getElementById("form-status").style.display = "block";
+    })
+    .catch((error) => {
+      alert("Oops! There was a problem submitting the form");
+    });
+}
